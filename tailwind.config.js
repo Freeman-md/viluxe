1dfb817e7bbf8ac13a736c4e0247b12ed79bf057
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2023-08-25 22:54:53 +0100

    UPDATE: update app theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    theme: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '0.25rem',
+          lg: '0.5rem',
+          xl: '1rem',
+          '2xl': '2rem',
+        },
+      },
+      extend: {
+        colors: {
+          primary: '#008080',
+          secondary: '#FF6B6B',
+        }
+      },
+    },
   },
   plugins: [],
 }
